af5cb6067a45a3e7fd7e3b091a3ffed6d17eb354
Davi Viana <davipviana@gmail.com>
2019-11-06 19:43:57 -0300

    Initial project

---
+++ src/main/java/com/davipviana/example/controller/UserController.java
@@ -0,0 +1,23 @@
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @GetMapping
+    public String getUsers(){
+        return "Retorno do metodo getUser - HTTP GET";
+    }
+ 
+    @PostMapping
+    public String createUser(){
+        return "Retorno do metodo createUser - HTTP POST";
+    }
+ 
+    @PutMapping
+    public String updateUser(){
+        return "Retorno do metodo updateUser - HTTP PUT";
+    }
+ 
+    @DeleteMapping
+    public String deleteUser(){
+        return "Retorno do metodo deleteUser - HTTP DELETE";
+    }
+}
